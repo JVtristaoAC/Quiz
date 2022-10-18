38a83742b3de5e1527eac7a9c09c0af099561d18
João Tristão <jvtristaoac@gmail.com>
2022-10-18 15:46:16 -0300

    Adicionar arquivos de projeto.

---
+++ Quiz/View/Login.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Quiz.View
+{
+    /// <summary>
+    /// Lógica interna para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_botao_Click(object sender, RoutedEventArgs e)
+        {
+            Quiz1 janela = new Quiz1();
+            janela.Show();
+            this.Close();
+        }
+    }
+}
